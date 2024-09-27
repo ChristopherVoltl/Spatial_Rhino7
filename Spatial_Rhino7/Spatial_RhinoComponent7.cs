ef9c90698bf48c4058a1ccd6d624ea4d5575f6cf
ChristopherVoltl <christopher.voltl@gmail.com>
2024-09-27 01:13:01 -0400

    Refactor curve processing and add PolylinePlaneDivider
    Updated `using` directive to include static import for `CurvePlaneDivider`. Modified `RegisterInputParams` and `RegisterOutputParams` to comment out certain parameters. Added `PolylinePlaneDivider` class with `DividePolylineByLength` method. Updated `SolveInstance` method to use `PolylinePlaneDivider` for dividing polylines and generating planes, adjusted input retrieval, and simplified output to only include `allPlanes` list.

--- Spatial_Rhino7/Spatial_RhinoComponent7.cs
+++ Spatial_Rhino7/Spatial_RhinoComponent7.cs
@@ -11,6 +11,7 @@ using Rhino.Commands;
 using System.Security.Cryptography;
 using Rhino.UI;
 using System.Net;
+using static Spatial_Rhino7.Spatial_RhinoComponent7.CurvePlaneDivider;
 
 namespace Spatial_Rhino7
 {
@@ -35,7 +36,7 @@ namespace Spatial_Rhino7
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddPlaneParameter("pathPlanes ", "pP", " an array of Planes", GH_ParamAccess.list);
+            //pManager.AddPlaneParameter("pathPlanes ", "pP", " an array of Planes", GH_ParamAccess.list);
             pManager.AddCurveParameter("pathCurves", "pC", " an array of Curves", GH_ParamAccess.list);
             pManager.AddNumberParameter("Parameter", "P", "Parameter to split the curves at (between 0 and 1)", GH_ParamAccess.item, 0.9);
         }
@@ -45,9 +46,9 @@ namespace Spatial_Rhino7
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddCurveParameter("Split Curves", "SC", "Resulting split curves", GH_ParamAccess.list);
+            //pManager.AddCurveParameter("PolyLines", "L", "Output Curves", GH_ParamAccess.list);
             pManager.AddPlaneParameter("Planes", "PL", "Planes created at division points", GH_ParamAccess.list);
-            pManager.AddPointParameter("SuperShape", "SS", "SuperShape for each plane", GH_ParamAccess.list);
+            //pManager.AddPointParameter("SuperShape", "SS", "SuperShape for each plane", GH_ParamAccess.list);
         }
 
         /// <summary>
@@ -89,25 +90,71 @@ namespace Spatial_Rhino7
 
                 return planes;
             }
+       
+
+
+        public class PolylinePlaneDivider
+        {
+            /// <summary>
+            /// Divides a polyline into equal parts based on the specified length and returns planes at the division points.
+            /// </summary>
+            /// <param name="polyline">The polyline to divide</param>
+            /// <param name="divisionLength">The length of each division</param>
+            /// <returns>A list of planes created at division points along the polyline</returns>
+            public static List<Plane> DividePolylineByLength(Curve polyline, double divisionLength)
+            {
+                List<Plane> planes = new List<Plane>();
+
+                if (polyline == null || divisionLength <= 0)
+                    return planes;
+
+      
+
+                // Divide the polyline curve into equal length segments
+                double[] divisionParams = polyline.DivideByLength(divisionLength, true);
+
+                if (divisionParams == null || divisionParams.Length == 0)
+                    return planes;
+
+                // Create planes at each division point
+                for (int i = 0; i < divisionParams.Length; i++)
+                {
+                    Point3d divisionPoint = polyline.PointAt(divisionParams[i]);
+
+
+                    Vector3d ZAxis = new Vector3d(0, 0, -1);
+                    // Create a plane at the division point with the tangent as an axis
+                    Plane plane = new Plane(divisionPoint, ZAxis);
+
+                    // Add the plane to the list
+                    planes.Add(plane);
+                }
+
+                return planes;
+            }
         }
 
+}
 
-        /// </summary>
-        /// 
 
-        protected override void SolveInstance(IGH_DataAccess DA)
+
+
+/// </summary>
+/// 
+
+protected override void SolveInstance(IGH_DataAccess DA)
         {
             // 1. Declare placeholder variables and assign initial invalid data.
             //    This way, if the input parameters fail to supply valid data, we know when to abort.
             List<Plane> pathPlanes = new List<Plane>();
-            var pathCurves = new List<Curve>();
+            List<Curve> pathCurves = new List<Curve>();
             double parameter = 0.5; // Default parameter
 
 
             // 2. Retrieve input data.
-            if (!DA.GetDataList(0, pathPlanes)) { return; }
-            if (!DA.GetDataList(1, pathCurves)) { return; }
-            if (!DA.GetData(2, ref parameter)) return;
+            //if (!DA.GetDataList(0, pathPlanes)) { return; }
+            if (!DA.GetDataList(0, pathCurves)) { return; }
+            if (!DA.GetData(1, ref parameter)) return;
 
             // 3. Abort on invalid inputs.
             if (pathCurves == null)
@@ -115,33 +162,8 @@ namespace Spatial_Rhino7
                 RhinoApp.WriteLine("The selected object is not a curve.");
             }
 
-            List<Curve> splitCurves = new List<Curve>();
-            List<Curve> orderedCurves = new List<Curve>();
             List<Point3d> pData_points = new List<Point3d>();
             List<Plane> allPlanes = new List<Plane>();
-
-            foreach (Curve curve in pathCurves)
-            {
-                if (curve == null || !curve.IsValid) continue;
-
-                // Get the curve's length and split it at the specified parameter
-                
-                curve.Domain = new Interval(0, 1); // Normalize the parameter if it's between 0 and 1
-                    
-                Curve[] splitResult = curve.Split(parameter); // Split the curve at the normalized parameter
-                if (splitResult == null || splitResult.Length != 2) continue;
-
-                // Process each resulting curve after the split
-                foreach (Curve segment in splitResult)
-                {
-                    if (segment == null || !segment.IsValid) continue;
-
-                    // Add the segment curve to the list
-                    splitCurves.Add(segment);
-                }
-            }
-
-
 
             //get the operation UI!
             int progIndex = smtPlugin.UIData.ProgramIndex;
@@ -189,68 +211,59 @@ namespace Spatial_Rhino7
                     SuperShape[] shapes = new SuperShape[1];
 
                     //Assinging SMT functions based in the length of the curve
-                    List<Curve> processedCurves = new List<Curve>();
                     List<SMTPData> pDataList = new List<SMTPData>();
 
                     SMTPData[] pData = new SMTPData[5];
-
+                    int counter = 0;
                     //loop through each path line or polyline 
-                    //for (int i = 0; i < pathCurves.Count; i++)
-                    //{
-                    //    if (pathCurves[i] == null || !pathCurves[i].IsValid) continue;
-
-
-                    //}
-                    //for each path, divide into planes
-
-
-                    //for each point, start extrusion, extrude path, end extrusion.
-
-                    Plane pathStart = pathPlanes[0];
-                    Plane pathEnd = pathPlanes[pathPlanes.Count - 1];
-                    //create the extrusion data
-                    pData[0] = new SMTPData(0, 0, 0, MoveType.Lin, pathStart, extrude, 1.0f);
-                    pData[1] = new SMTPData(1, 0, 0, MoveType.Lin, pathStart, cool, 1.0f);
-
-                    pDataList.Add(pData[0]);
-                    pDataList.Add(pData[1]);
-                    // Loop through each curve in the list
-                    for (int i = 0; i < pathPlanes.Count; i++)
+                    for (int j = 0; j < pathCurves.Count; j++)
                     {
-                        if (pathPlanes[i] == null || !pathPlanes[i].IsValid) continue;
+                        if (pathCurves[j] == null || !pathCurves[j].IsValid) continue;
+
+                        //get the planes for each path
+                        List<Plane> crvPathPlanes = PolylinePlaneDivider.DividePolylineByLength(pathCurves[j], 10);                     
 
 
-                        Plane path = pathPlanes[i];
+                        //for each point, start extrusion, extrude path, end extrusion.
 
-                        pData[2] = new SMTPData(i+2, 0, 0, MoveType.Lin, path, 1.0f);
+                        Plane pathStart = crvPathPlanes[0];
+                        Plane pathEnd = crvPathPlanes[crvPathPlanes.Count - 1];
+                        //create the extrusion data
+                        pData[0] = new SMTPData(counter, 0, 0, MoveType.Lin, pathStart, extrude, 1.0f);
+                        counter++;
+                        pData[1] = new SMTPData(counter + 1, 0, 0, MoveType.Lin, pathStart, cool, 1.0f);
+                        counter++;
 
+                        pDataList.Add(pData[0]);
+                        pDataList.Add(pData[1]);
 
+                        allPlanes.Add(pathStart);
 
-                        //pData[1] = new SMTPData(5, 5, 5, MoveType.Joint, place, stopExtrude, 1.0f);
-                        //pData[4] = new SMTPData(6, 6, 6, MoveType.Joint, safe1, 1.0f);
+                        // Loop through each curve in the list
+                        for (int i = 0; i < crvPathPlanes.Count; i++)
+                        {
+                            if (crvPathPlanes[i] == null || !crvPathPlanes[i].IsValid) continue;
 
-                        //finished with path
-                        //Guid guid = Guid.NewGuid();
-                        //smtPlugin.UserData[guid] = pData;
-                        //pData_points.Add(pData[0].Origin);
-                        //allPlanes.Add(pathPlanes[i]);
-                        //pDataList.Add(pData[0]);
-                        pDataList.Add(pData[2]);
-                        
+                            Plane path = crvPathPlanes[i];
 
+                            pData[2] = new SMTPData(counter + 2, 0, 0, MoveType.Lin, path, 1.0f);
 
-                        //pData_points.Add(pData[0].Origin);
-                        //shapes[0] = SuperShape.SuperShapeFactory(guid, null, DivisionStyle.PointData, ZOrientStyle.PointData, VectorStyle.ByParam, YOrientStyle.PointData, false, 0.0, Rhino.Geometry.Plane.WorldXY);
-                        //smtPlugin.UserGeometry[guid] = partObjs[i].ExtrusionGeometry;
+                            pDataList.Add(pData[2]);
 
+                            allPlanes.Add(path);
+                            counter++;
+                        }
+
+                        pData[3] = new SMTPData(counter, 0, 0, MoveType.Lin, pathEnd, stopCooling, 1.0f);
+                        counter++;
+                        pData[4] = new SMTPData(counter, 0, 0, MoveType.Lin, pathEnd, stopExtrude, 1.0f);
+                        counter++;
+
+                        pDataList.Add(pData[3]);
+                        pDataList.Add(pData[4]);
+
+                        allPlanes.Add(pathEnd);    
                     }
-
-                    pData[3] = new SMTPData(pathPlanes.Count + 1, 0, 0, MoveType.Lin, pathEnd, stopCooling, 1.0f);
-                    pData[4] = new SMTPData(pathPlanes.Count + 2, 0, 0, MoveType.Lin, pathEnd, stopExtrude, 1.0f);
-
-
-                    pDataList.Add(pData[3]);
-                    pDataList.Add(pData[4]);
 
                     //store all the pointdata and then instantiate the shape outside of the loop
                     Guid guid = Guid.NewGuid();
@@ -277,9 +290,8 @@ namespace Spatial_Rhino7
                 RhinoApp.WriteLine("You must select an Operation");
 
             // 3. Set the outputs
-            DA.SetDataList(0, orderedCurves);
-            DA.SetDataList(1, allPlanes);
-            DA.SetDataList(2, pData_points);
+            DA.SetDataList(0, allPlanes);
+            //DA.SetDataList(2, pData_points);
 
         }
 
